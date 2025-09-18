a91f230f548e9ff1af1524aae7ec78fe3de3d85f
DeaconBlues64 <oscarangleberger2024@gmail.com>
2025-09-17 21:27:59 -0400

    SOME scene transition stuff working
    Stuff like entry points needs to be figured out still, but this works kinda

---
+++ transition.gd
@@ -0,0 +1,17 @@
+extends Area3D
+@export var target_scene:PackedScene 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_packed(target_scene)
+	pass # Replace with function body.
