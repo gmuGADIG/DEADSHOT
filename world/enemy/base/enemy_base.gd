7e17f9188ed5b35de6e2f01ab6b6d2598a0e5451
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2025-09-17 21:17:42 -0400

    implemented basic enemy pathfinding

--- world/enemy/base/enemy_base.gd
+++ world/enemy/base/enemy_base.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 
+@export var movement_speed : float = 10.0
+@onready var navigation_agent : NavigationAgent3D = $NavigationAgent3D
+@export var player : CharacterBody3D # replace with better option later
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,5 +10,13 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	navigation_agent.target_position = player.position
+	var next_position : Vector3 = navigation_agent.get_next_path_position()
+	var direction : Vector3 = global_position.direction_to(next_position)
+	velocity = direction * movement_speed
+	move_and_slide()
+	pass
+
+func navigate() -> void:
 	pass
