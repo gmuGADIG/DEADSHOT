1c70a74461b43c6402a6791e926fdd0ad37678c5
Justin1L8 <justin.w.langdon@gmail.com>
2025-09-24 16:59:25 -0400

    Corrected naming convention, cleaned code

--- world/player/player.gd
+++ world/player/player.gd
@@ -1,51 +1,48 @@
 extends CharacterBody3D
 
 ## EXPORT VARIABLES
-# Movement
-@export var walkSpeed: float = 8.0
-@export var rollSpeed: float = 18.0
-## Controls how much player input affects steering when mid-roll. 
-@export var rollInfluence: float = 8
+@export var walk_speed: float = 8.0
 
-## This timer node controls the amount of time rolling takes.
-@export var rollDurationTimer: Timer
+@export var roll_speed: float = 18.0
+@export var roll_duration: float = 0.4
+@export var roll_influence: float = 8 ## Controls how much player input affects steering when mid-roll. 
 
 ## These are the states that the player can be in. States control what the player can do.
-## So far, they're just pertaining to movement. They might not always be, so I'm not calling it movementStates.
-enum playerStates{
+enum PlayerState {
 	WALKING,
 	ROLLING
 }
-## This variable holds the current state. By default, the player will be walking
-var currentState: playerStates = playerStates.WALKING
 
+var current_state: PlayerState = PlayerState.WALKING
+
+## Returns the inputted walking direction on the XZ plane (Y = 0)
+func walking_dir() -> Vector3:
+	var input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	return Vector3(input.x, 0, input.y)
 
 func _physics_process(_delta: float) -> void:
-	var input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var movement := Vector3(input.x, 0, input.y)
-	
 	if Input.is_action_just_pressed("roll"):
-		beginRoll()
+		begin_roll()
 	
-	match currentState:
-		playerStates.WALKING:
-			velocity = movement * walkSpeed
-		playerStates.ROLLING:
+	match current_state:
+		PlayerState.WALKING:
+			velocity = walking_dir() * walk_speed
+		PlayerState.ROLLING:
 			## We move the velocity vector towards the direction of the movement. 
 			## This means that velocity doesn't immediately become where we're pointing, but changes over time.
 			## We normalize the shit out of everything so we can multiply it by a consistent speed.
 			## This way there's no weird acceleration or slowdown.
-			velocity = velocity.move_toward(movement.normalized(), rollInfluence).normalized() * rollSpeed
+			velocity = velocity.move_toward(walking_dir().normalized(), roll_influence).normalized() * roll_speed
 			# TODO: Rolling while not holding any other inputs effectively freezes you.
 			# The previous facing direction should be considered. 
 			
 	move_and_slide()
 
-func beginRoll() -> void:
+func begin_roll() -> void:
 	#TODO: Play animation, do iframes.
-	currentState = playerStates.ROLLING
-	rollDurationTimer.start()
-	await rollDurationTimer.timeout
-	currentState = playerStates.WALKING
+	current_state = PlayerState.ROLLING
+	%RollDurationTimer.start()
+	await %RollDurationTimer.timeout
+	current_state = PlayerState.WALKING
 	
 	
