c459642cb34c96bd5f44c055ec8c1ce8afceb2a1
Justin1L8 <justin.w.langdon@gmail.com>
2025-09-24 17:08:25 -0400

    Added more typoes

--- world/enemy/meat/meat.gd
+++ world/enemy/meat/meat.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	print("Currecny Collected")
+	print("Currecny Colletced")
 	Global.meat_currency += 1
 	print(Global.meat_currency)
 	queue_free()
