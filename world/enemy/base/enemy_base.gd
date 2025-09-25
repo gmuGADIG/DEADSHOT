937ce8a13196c17ce36775a017d33c166cf765c2
Camber Boles <camberb51@gmail.com>
2025-09-24 20:48:52 -0400

    Added enums for enemy type, aggro status, and state change logic based on radius

--- world/enemy/base/enemy_base.gd
+++ world/enemy/base/enemy_base.gd
@@ -1,8 +1,39 @@
 extends CharacterBody3D
 
+## Controls what the enemy does when benign.
+enum EnemyType {
+	## The enemy stays still until the player enters aggro range.
+	IDLE,
+	## The enemy moves through a set list of points.
+	PATROLLING
+}
+
+## Controls the current state of the enemy's aggro.
+enum AggroState {
+	## The enemy has not noticed the player.
+	BENIGN,
+	## The enemy has detected a player within the smell range.
+	SCOUTING,
+	## The enemy has sighted the player and is actively tracking.
+	TRACKING
+}
+
 ## Controls the speed of the enemy agent.
 @export var movement_speed : float = 10.0
-@export var player : CharacterBody3D # replace with better option later
+
+## The player in the scene.
+@export var player : Node3D # replace with better option later
+
+## The maximum distance at which the enemy will begin tracking the player.
+@export var sight_radius : float = 10.0
+
+## The maximum distance at which the enemy will begin scouting for the player.
+@export var smell_radius : float = 15.0
+
+## Does the enemy remain still or move about on their own?
+@export var type : EnemyType = EnemyType.IDLE;
+var aggro : AggroState = AggroState.BENIGN;
+var player_distance : float;
 
 @onready var navigation_agent : NavigationAgent3D = $NavigationAgent3D
 
@@ -15,17 +46,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	
-	# TODO: figure out whether or not it's computationally worth it to set the
-	# target every physics frame.
-	
-	# Get the position to the next path checkpoint, then point velocity towards
-	# the checkpoint.
-	var next_position : Vector3 = navigation_agent.get_next_path_position()
-	var direction : Vector3 = global_position.direction_to(next_position)
-	velocity = direction * movement_speed
-	
-	move_and_slide()
+	player_distance = global_position.distance_to(player.global_position);
+	if player_distance > smell_radius:
+		# Player is far away
+		aggro = AggroState.BENIGN;
+	elif player_distance > sight_radius:
+		# Player is within smell radius
+		aggro = AggroState.SCOUTING;
+	else:
+		# Player is within sight radius
+		aggro = AggroState.TRACKING;
 	pass
 
 ## Sets the movement target of this enemy agent.
@@ -33,5 +63,15 @@ func set_movement_target(movement_target: Vector3) -> void:
 	navigation_agent.target_position = movement_target
 	pass
 
+## Moves the enemy towards the player.
 func navigate() -> void:
+	# TODO: figure out whether or not it's computationally worth it to set the
+	# target every physics frame.
+	set_movement_target(player.position)
+	# Get the position to the next path checkpoint, then point velocity towards
+	# the checkpoint.
+	var next_position : Vector3 = navigation_agent.get_next_path_position()
+	var direction : Vector3 = global_position.direction_to(next_position)
+	velocity = direction * movement_speed
+	move_and_slide()
 	pass
