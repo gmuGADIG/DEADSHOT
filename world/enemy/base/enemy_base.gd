ff24f14594ae5193b3675b09e8c6f90fec915d45
Camber Boles <camberb51@gmail.com>
2025-09-24 21:31:09 -0400

    Added code regions and patrol path array

--- world/enemy/base/enemy_base.gd
+++ world/enemy/base/enemy_base.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+#region Enumerations
 ## Controls what the enemy does when benign.
 enum EnemyType {
 	## The enemy stays still until the player enters aggro range.
@@ -17,29 +18,42 @@ enum AggroState {
 	## The enemy has sighted the player and is actively tracking.
 	TRACKING
 }
+#endregion
 
+#region Variables
+## The player in the scene.
+@export var player : Node3D
+
+@export_group("Enemy Stats")
 ## Controls the speed of the enemy agent.
 @export var movement_speed : float = 10.0
-
-## The player in the scene.
-@export var player : Node3D # replace with better option later
-
 ## The maximum distance at which the enemy will begin tracking the player.
 @export var sight_radius : float = 10.0
-
 ## The maximum distance at which the enemy will begin scouting for the player.
 @export var smell_radius : float = 15.0
-
 ## Does the enemy remain still or move about on their own?
-@export var type : EnemyType = EnemyType.IDLE;
+@export var type : EnemyType = EnemyType.IDLE
+## Does the enemy return to their original position after the player leaves?
+@export var returns_to_post : bool = true;
+## The starting position of the enemy.
+@export var starting_pos : Vector3;
+## The positions to cycle through when patrolling.
+@export var patrol_path : Array[Vector3];
+var patrol_index : int = 0;
+
+## Current aggro state of the enemy
 var aggro : AggroState = AggroState.BENIGN;
+## Current distance to the player
 var player_distance : float;
+## The points that the enemy moves betweeen when patrolling
 
 @onready var navigation_agent : NavigationAgent3D = $NavigationAgent3D
+#endregion
 
 # TODO: Facilitate different modes of movement, such as patrolling.
 # Refer to the GDD for all possible modes of movement.
 
+#region Builtin Functions
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -68,7 +82,9 @@ func _physics_process(_delta: float) -> void:
 			scout();
 		AggroState.TRACKING:
 			track();
+#endregion
 
+#region Behaviour Functions
 ## Sets the movement target of this enemy agent.
 func set_movement_target(movement_target: Vector3) -> void:
 	navigation_agent.target_position = movement_target
@@ -78,6 +94,7 @@ func idle() -> void:
 	pass
 
 func patrol() -> void:
+	#if global_position.distance_to(patrol_path[patrol_index]) < 
 	pass
 
 func scout() -> void:
@@ -95,3 +112,4 @@ func track() -> void:
 	velocity = direction * movement_speed
 	move_and_slide()
 	pass
+#endregion
