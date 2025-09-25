f8085c5d129a0958a1fd27c5b35029aba2136e52
KaratePikachu <KaidenKeyZ@gmail.com>
2025-09-24 21:27:09 -0400

    Save System

--- world/player/player.gd
+++ world/player/player.gd
@@ -1,6 +1,20 @@
-extends CharacterBody3D
+class_name Player extends CharacterBody3D
 
 const SPEED = 6.5
+
+static var instance : Player
+
+func _ready() -> void:
+	print("player ready")
+	instance = self
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		Save.save_game()
+	
+	if event.is_action_pressed("roll"):
+		print("Spacec")
+		Save.load_game()
 
 func _physics_process(_delta: float) -> void:
 	var input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
