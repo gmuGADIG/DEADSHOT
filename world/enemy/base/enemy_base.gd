cb1ec175ab0d21252397ed9afc24c296409d91c4
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2025-09-26 23:07:45 -0400

    implemented some idle-type behavior
    TODO:
    - fix issues with scouting (namely enemy patience)
    - extract distance tolerance (currently 5) to exposed(?) variable
    - implement patrolling

--- world/enemy/base/enemy_base.gd
+++ world/enemy/base/enemy_base.gd
@@ -29,14 +29,22 @@ enum AggroState {
 @export var movement_speed : float = 10.0
 ## The maximum distance at which the enemy will begin tracking the player.
 @export var sight_radius : float = 10.0
-## The maximum distance at which the enemy will begin scouting for the player.
+## The maximum distance at which the enemy will begin scouting for the player
+## after having lost them.
 @export var smell_radius : float = 15.0
 ## Does the enemy remain still or move about on their own?
 @export var type : EnemyType = EnemyType.IDLE
+## The amount of time (seconds) the enemy stays at the last known player 
+## position before returning back to what it was doing.
+@export var patience : float = 5.0;
+
+@export_subgroup("Idle-Only Settings")
 ## Does the enemy return to their original position after the player leaves?
 @export var returns_to_post : bool = true;
 ## The starting position of the enemy.
 @export var starting_pos : Vector3;
+
+@export_subgroup("Patrol-Only Settings")
 ## The positions to cycle through when patrolling.
 @export var patrol_path : Array[Vector3];
 var patrol_index : int = 0;
@@ -45,17 +53,21 @@ var patrol_index : int = 0;
 var aggro : AggroState = AggroState.BENIGN;
 ## Current distance to the player
 var player_distance : float;
-## The points that the enemy moves betweeen when patrolling
 
 @onready var navigation_agent : NavigationAgent3D = $NavigationAgent3D
-#endregion
 
-# TODO: Facilitate different modes of movement, such as patrolling.
-# Refer to the GDD for all possible modes of movement.
+## The last known position of the player.
+var last_known_player_position : Vector3;
+
+var shouldMove : bool = false;
+var wasTracking : bool = false;
+
+#endregion
 
 #region Builtin Functions
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	last_known_player_position = player.position
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -66,22 +78,36 @@ func _process(_delta: float) -> void:
 		aggro = AggroState.BENIGN;
 	elif player_distance > sight_radius:
 		# Player is within smell radius
-		aggro = AggroState.SCOUTING;
+		if wasTracking:
+			aggro = AggroState.SCOUTING;
 	else:
 		# Player is within sight radius
 		aggro = AggroState.TRACKING;
 	pass
 
 func _physics_process(_delta: float) -> void:
+	print(wasTracking)
 	match aggro:
 		AggroState.BENIGN when type == EnemyType.IDLE:
+			print("Idle!")
 			idle();
 		AggroState.BENIGN when type == EnemyType.PATROLLING:
+			print("Patrolling!")
 			patrol();
 		AggroState.SCOUTING:
+			print("Scouting!")
 			scout();
 		AggroState.TRACKING:
+			print("TRACKING!")
 			track();
+	
+	# Get the position to the next path checkpoint, then point velocity towards
+	# the checkpoint.
+	if (shouldMove):
+		var next_position : Vector3 = navigation_agent.get_next_path_position()
+		var direction : Vector3 = global_position.direction_to(next_position)
+		velocity = direction * movement_speed
+		move_and_slide()
 #endregion
 
 #region Behaviour Functions
@@ -91,25 +117,46 @@ func set_movement_target(movement_target: Vector3) -> void:
 	pass
 
 func idle() -> void:
+	
+	wasTracking = false
+	
+	# if the enemy returns to post,
+		# if the enemy aint there yet,
+			# the enemy should move
+		# else,
+			# the enemy should not move
+	# if the enemy does not return to post
+		# the enemy should not move
+		
+	if returns_to_post:
+		set_movement_target(starting_pos)
+		if (global_position.distance_to(starting_pos) > 5):
+			print("greater than tolerance, let's go")
+			shouldMove = true
+		else:
+			shouldMove = false
+	else:
+		shouldMove = false
 	pass
 
 func patrol() -> void:
+	wasTracking = false
+	shouldMove = true
 	#if global_position.distance_to(patrol_path[patrol_index]) < 
 	pass
 
 func scout() -> void:
+	shouldMove = true
+	set_movement_target(last_known_player_position)
+	# wait some amount of seconds
+	aggro = AggroState.BENIGN
 	pass
 
 ## Moves the enemy towards the player.
 func track() -> void:
-	# TODO: figure out whether or not it's computationally worth it to set the
-	# target every physics frame.
+	wasTracking = true
+	shouldMove = true
 	set_movement_target(player.position)
-	# Get the position to the next path checkpoint, then point velocity towards
-	# the checkpoint.
-	var next_position : Vector3 = navigation_agent.get_next_path_position()
-	var direction : Vector3 = global_position.direction_to(next_position)
-	velocity = direction * movement_speed
-	move_and_slide()
+	last_known_player_position = player.position
 	pass
 #endregion
