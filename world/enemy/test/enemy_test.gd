37cc977939b2ca0b7d186c0377a1a02df464d109
Ethan Nguyen <72627687+cooki-chan@users.noreply.github.com>
2025-10-01 21:56:47 -0400

    fixed damage after invuln timer

--- world/enemy/test/enemy_test.gd
+++ world/enemy/test/enemy_test.gd
@@ -3,4 +3,9 @@ extends CharacterBody3D
 func _on_damage_area_body_entered(body: Node3D) -> void:
 	if body is Player:
 		body.hurt(1)
-	print("GET REKT")
+
+func _physics_process(_delta: float) -> void:
+	if $DamageArea.has_overlapping_bodies():
+		for y: Node3D in $DamageArea.get_overlapping_bodies():
+			if y is Player:
+				y.hurt(1)
