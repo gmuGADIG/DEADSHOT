016d5b97251a35e9b88a4a6c14f3fa9d321776e0
Justin1L8 <justin.w.langdon@gmail.com>
2025-10-08 18:25:04 -0400

    Reverted mistake in health refactor

--- systems/attacking/hurtbox/hurtbox_component.gd
+++ systems/attacking/hurtbox/hurtbox_component.gd
@@ -1,16 +1,11 @@
 class_name HurtboxComponent extends Area3D
 
 @export var health_component : HealthComponent
-@export var is_player: bool ## True if this hurtbox is on the player. Attacks will filter accordingly.
+@export var allowed_damage_sources : Array[DamageInfo.Source]
 
 func _ready() -> void:
 	assert(health_component != null, "No health component added")
 
-func hit(damage: DamageInfo) -> void:
-	# check if sources allows damage
-	if is_player:
-		if damage.source == DamageInfo.Source.PLAYER: return
-	else:
-		if damage.source == DamageInfo.Source.ENEMY: return
-	
-	health_component.hurt(damage.damage)
+func hit(dmg: DamageInfo) -> void:
+	if dmg.source in allowed_damage_sources:
+		health_component.hurt(dmg.damage)
