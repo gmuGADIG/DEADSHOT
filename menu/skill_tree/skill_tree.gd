e72d236fdd0fc95a7daeb418288d8d55f30e73c4
OnlyMondays <rahul.mathew9000@gmail.com>
2025-10-08 21:15:56 -0400

    Basic structure for skill tree
    Specifications for the skill tree functions in progress, placeholders everywhere currently.

---
+++ menu/skill_tree/skill_tree.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _on_skill_tree_upgrade_pressed() -> void:
+	# Should display info of the upgrade pressed
+	pass;
+
+func _on_confirm_upgrade_button_pressed() -> void:
+	# Should confirm the upgrade, changes should be reflected in player stats
+	pass;
+
+func _on_exit_button_pressed() -> void:
+	# Closes skill tree menu, returns to pause screen
+	pass;
+
+func _disable_upgrade_upon_path_selected() -> void:
+	# Once a player selects a weapon path, other weapon paths should be inaccessable
+	pass;
