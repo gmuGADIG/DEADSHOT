a30e343f02be2698cf390b98b353494990fea6c6
Shakeeb Uddin <shakeebu834@gmail.com>
2025-10-15 21:33:01 -0400

    Tried adding stuff to skill button

--- skill_button.gd
+++ skill_button.gd
@@ -1,7 +1,18 @@
 extends TextureButton
+class_name Skill_Button
 
 @export var skillName:String
 @export var meatCost:int
+@onready var Skill_Branch : Line2D = $Skill_Branch
 
 func _ready() -> void:
 	$Label.text = skillName
+	# Fix this, make sure line goes in correct place
+	if get_parent() is Skill_Button:
+		Skill_Branch.add_point(self.global_position + self.size/2)
+		Skill_Branch.add_point(get_parent().global_position + self.size/2)
+
+
+func _on_pressed() -> void:
+	print(skillName)
+	#if player level or prev skill is acheived, allow it, or deny it
