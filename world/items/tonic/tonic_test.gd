2414deef5a2c1d729e474571f62a82940ce3f2bf
Camber Boles <camberb51@gmail.com>
2025-10-15 20:27:33 -0400

    Tonic can heal the player now

--- world/items/tonic/tonic_test.gd
+++ world/items/tonic/tonic_test.gd
@@ -3,5 +3,7 @@ extends Item
 func on_pickup(body: Node3D) -> void:
 	var player: Player = body as Player;
 	if player:
-		print("I GOT EATEN!");
+		var player_health : Health = player.get_node("HurtboxComponent/HealthComponent");
+		player_health.heal(2);
+		print(player_health.health);
 		queue_free();
