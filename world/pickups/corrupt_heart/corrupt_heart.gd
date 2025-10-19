4de0c0d317ad3fa702df8e2f29adbb269e7916ed
KaratePikachu <80354180+KaratePikachu@users.noreply.github.com>
2025-10-19 01:38:26 -0400

    Funky particles

--- world/pickups/corrupt_heart/corrupt_heart.gd
+++ world/pickups/corrupt_heart/corrupt_heart.gd
@@ -1,1 +1,49 @@
 extends Node3D
+
+@export var health : Health
+
+@export var death_animation_time : float = 3.0
+
+@export var blood_burst_packed : PackedScene
+@export var final_burst_packed : PackedScene
+
+var dying : bool = false
+var death_time : float
+func _ready() -> void:
+	
+	
+	health.killed.connect(on_death)
+	$AnimationPlayer.play("corrupt_heart")
+
+func _process(delta: float) -> void:
+	if dying:
+		death_time += delta
+		$Sprite3D.position.x = 0.1*sin(death_time*50)
+
+func on_death() -> void:
+	dying = true
+	$AnimationPlayer.pause()
+	
+	##Steady stream of blood
+	var blood_burst : GPUParticles3D = blood_burst_packed.instantiate()
+	blood_burst.lifetime = death_animation_time
+	add_sibling(blood_burst)
+	blood_burst.global_position = $BloodEmitCenter.global_position
+	blood_burst.emitting = true
+	
+	
+	var tween : Tween = get_tree().create_tween().set_parallel(true)
+	
+	tween.tween_property($Sprite3D,"scale", Vector3(0.85,0.85,0.85),death_animation_time).set_trans(Tween.TRANS_BOUNCE)
+	
+	
+	
+	await get_tree().create_timer(death_animation_time).timeout
+	
+	##Final Burst of blood
+	var final_burst : GPUParticles3D = final_burst_packed.instantiate()
+	add_sibling(final_burst)
+	final_burst.global_position = $BloodEmitCenter.global_position
+	final_burst.emitting = true
+	queue_free()
+	
