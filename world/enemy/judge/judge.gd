6c64dc96b9b50327c2db1605c8c32843a696bd76
jwexton <jwexton@gmu.edu>
2025-10-22 21:56:04 -0400

    Created judge.tscn and made it chase the player, next up is coding its attacks

--- world/enemy/judge/judge.gd
+++ world/enemy/judge/judge.gd
@@ -5,17 +5,12 @@ extends EnemyBase
 
 #region Behaviour Functions
 
-func _init()->void:
-	max_hp = 100 #Obviously these are subject to change
-	damage = 5  #Don't know how much HP the player has rn so consider these placeholders
-
-
-func hostile()->void:
+func hostile() -> void:
 	set_movement_target(player.global_position);
 	should_move = not is_close_to_destination();
 	#Took this from enemy_test.gd, I figure that an aggressive enemy
 	#would pursue you in order to shoot you with a shotgun, a short-range weapon
 
-func attack()->void:
+func attack()-> void:
 	pass
 #endregion
