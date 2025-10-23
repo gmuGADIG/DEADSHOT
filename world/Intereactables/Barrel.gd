72622dd881ad12a8ca6cdcd49455ce4b6f039f22
DuoBlade-birb <martellcollins2007@gmail.com>
2025-10-22 20:26:17 -0400

    10/22/2025 (FINISH)
    Finished the animated sprites, I tried to get it to disappear when hitting a wall but I'm leaving that to a man smarted than me

--- world/Intereactables/Barrel.gd
+++ world/Intereactables/Barrel.gd
@@ -1,10 +1,14 @@
 extends CharacterBody3D
 
+@onready var _animated_sprite := $AnimatedSprite3D 
+
+
 func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
 func hit(bullet: Bullet) -> void:
+	_animated_sprite.set_frame_and_progress(1,0.0)
 	if bullet.velocity.angle_to(Vector3.FORWARD) > 2 :
 		velocity.z = 3
 	if bullet.velocity.angle_to(Vector3.BACK) > 2 :
@@ -14,4 +18,5 @@ func hit(bullet: Bullet) -> void:
 	if bullet.velocity.angle_to(Vector3.RIGHT) > 2 :
 		velocity.x = -3
 	bullet.queue_free()
-	
+	await get_tree().create_timer(5.0).timeout
+	_animated_sprite.set_frame_and_progress(2,0.0)
--- world/Intereactables/BarrelAnim.gd
+++ world/Intereactables/BarrelAnim.gd
@@ -1,9 +1,1 @@
 extends AnimatedSprite3D
-
-var Barrel = AnimatedSprite3D.new()
-
-func hit(bullet: Bullet) -> void:
-	Barrel.play("new_animation")
-	
-func _process(delta):
-	pass
