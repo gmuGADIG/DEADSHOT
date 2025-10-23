bd7a902e857269edc0fd3d82133fdad27b6f09d5
Camber Boles <camberb51@gmail.com>
2025-10-22 20:24:34 -0400

    Variables and setter function added for ammo label

--- world/player/HUD/hud.gd
+++ world/player/HUD/hud.gd
@@ -8,12 +8,18 @@ extends Control
 # It's a child of the Player so that it can quickly update the HUD. 
 # For getting non-players to talk to the HUD, give the non-player object a signal and connect it to the HUD.
 
-@export var stamina_bar: ProgressBar ## The stamina bar itself, which Player tells the stamina value.
-@export var stamina_container: PanelContainer ## The stamina bar's parent that fades in and out.
+## The stamina bar itself, which Player tells the stamina value.
+@onready var stamina_bar: ProgressBar = $StaminaContainer/ProgressBar
+## The stamina bar's parent that fades in and out.
+@onready var stamina_container: PanelContainer = $StaminaContainer
+## The container for the Ammo tracker.
+@onready var ammo_container: PanelContainer = $AmmoContainer
+## The ammo tracker text label.
+@onready var ammo_label: RichTextLabel = $AmmoContainer/Label
 
 func _ready() -> void:
 	# Stamina bar is hidden by default until player enters a Combat Encounter.
-	$StaminaContainer.modulate.a = 0.0
+	stamina_container.modulate.a = 0.0
 
 func fade_stamina_out() -> void:
 	var tween: Tween = get_tree().create_tween()
@@ -23,5 +29,10 @@ func fade_stamina_in() -> void:
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(stamina_container, "modulate:a", 1.0, 0.1)
 
+## Updates the stamina bar with the given amount.
 func update_stamina_bar(amount: float) -> void:
 	stamina_bar.value = amount
+
+## Replaces the ammo label text in the HUD with the given string. 
+func update_ammo_text(text: String) -> void:
+	ammo_label.text = text;
