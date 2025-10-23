7d8b32b6b5e6872c21c321111b74d4bd78b61ab3
Kernighan <107226662+King5ive@users.noreply.github.com>
2025-10-22 21:59:59 -0400

    Completion
    Longhorn charges towards towards the player and (probably) deals damage

--- world/enemy/longhorn/longhorn.gd
+++ world/enemy/longhorn/longhorn.gd
@@ -4,9 +4,9 @@ extends EnemyBase
 @export var timer:Timer
 @export var charge_time:float
 @export var cooldown_time:float
-@export var hurtbox:Hurtbox
 @export var health:Health
-var player_damage_source:DamageInfo.Source = DamageInfo.Source.PLAYER
+@export var atk_source: DamageInfo.Source
+@export var atk_knockback: DamageInfo.KnockbackStrength
 var target:Vector3
 
 #endregion
@@ -17,7 +17,6 @@ func hostile() -> void:
 	if timer.is_stopped():
 		print("Charging")
 		health.vulnerable = false
-		#hurtbox.allowed_damage_sources.pop_front()
 		timer.start(charge_time)
 		
 
@@ -43,3 +42,14 @@ func _on_charge_timer_timeout() -> void:
 		aggro = AggroState.ATTACKING
 	elif aggro == AggroState.ATTACKING:
 		aggro = AggroState.HOSTILE
+
+
+
+func _on_hurter_box_area_entered(area: Area3D) -> void:
+	if area is Hurtbox:
+		var hurtbox : Hurtbox = area
+		var dmg := DamageInfo.new(damage, atk_source, atk_knockback, velocity.normalized())
+		var did_damage := hurtbox.hit(dmg)
+		print(hurtbox.get_parent())
+		if did_damage:
+			print("owie")
