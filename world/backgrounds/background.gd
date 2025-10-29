96d9c9b642720e9abd8078ea0ee45deac17e08c5
Justin1L8 <justin.w.langdon@gmail.com>
2025-10-29 17:00:55 -0400

    Adjusted background

--- world/backgrounds/background.gd
+++ world/backgrounds/background.gd
@@ -5,4 +5,6 @@ func _process(_delta: float) -> void:
 	
 	position = cam.global_position
 	position += -cam.basis.z * 50.0
-	position.x *= 0.9
+	position += cam.basis.y * 10.0
+	position.x *= 0.8
+	position.z *= 1.3
