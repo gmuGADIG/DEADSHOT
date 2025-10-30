1b79e3b13c0282862fe7f392faaf9e08a043f60b
KaratePikachu <KaidenKeyZ@gmail.com>
2025-10-29 23:48:21 -0400

    spike wip

--- world/enemy/bosses/mass/spike_attack.gd
+++ world/enemy/bosses/mass/spike_attack.gd
@@ -1,8 +1,16 @@
-extends Node3D
+class_name Spike extends Node3D
 
-@export var charge_time : float
+@export var warning_spin_speed : float
+@export var spike_curve : Curve
 @export var damage : DamageInfo
 
-func _ready() -> void:
+var age : float = 0
+
+
+func _process(delta: float) -> void:
+	$WarningTarget.rotate(Vector3.UP,warning_spin_speed*delta)
+	$Spike.position.y = spike_curve.sample(age)
+	age+= delta
+	if age >= spike_curve.max_domain:
+		queue_free()
 	
-	pass
--- world/enemy/bosses/mass/the_mass.gd
+++ world/enemy/bosses/mass/the_mass.gd
@@ -1,4 +1,9 @@
-extends BossEnemy
+class_name TheMass extends BossEnemy
+
+@export_category("Spike Attack")
+@export var spike : PackedScene
+@export var spike_distance_multiplier : float
+
 
 func pick_action() -> void:
 	if $Health.health > $Health.max_health/2:
@@ -10,4 +15,7 @@ func idle() -> void:
 	pass
 
 func spike_attack() -> void:
-	print("spiker")
+	var spike_pos : Vector3 = Player.instance.global_position+Player.instance.velocity*spike_distance_multiplier
+	var new_spike : Spike = spike.instantiate()
+	add_sibling(new_spike)
+	new_spike.global_position = spike_pos
