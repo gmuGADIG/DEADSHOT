e019a77fbb872793829c21cd0e0891a6b629a644
Justin Langdon <justin.w.langdon@gmail.com>
2025-11-05 17:24:05 -0500

    Improved campfire visuals and menu

--- world/interactables/campfire/campfire.gd
+++ world/interactables/campfire/campfire.gd
@@ -1,34 +1,32 @@
 extends Interactable
 
-@export var health_component : Health #Might be irrelevant?
-@onready var max_health: int = Player.instance.health_component.max_health #Gets the max health index from player
-static var usedList: Array = [] #Global array of all lit campfires
-var extinguish: bool = false #Boolean if campfire is interacted with
+static var used_list: Array = [] ## Global array of all lit campfires
+var extinguish: bool = false ## Boolean if campfire is interacted with
+
 func _ready() -> void:
-	$CampfireMenu.hide()#Hides menu, control node
-	
-	if(usedList.has(get_path())): #Checks if campfire is in global array
-		extinguish = true; #Extinguishes campfire
+	if(used_list.has(get_path())): # Checks if campfire is in global array
+		extinguish = true # Extinguishes campfire
 		
-	if(extinguish): #Checks if campfire is extinguished, see above
-		$campfireSprite.play('extinguished')
+	if(extinguish): # Checks if campfire is extinguished, see above
+		%Sprite.play('extinguished')
 	else:
-		$campfireSprite.play('lit')
-	
+		%Sprite.play('lit')
+
 func interact()->void:
+	if CampfireMenu.instance != null:
+		return # menu's already open; ignore
 	
-	extinguish = true #Campfire has been interacted with
-	if not usedList: #If this is a new campifre will add to array
-		usedList.append(get_path())
-	$CampfireMenu.visible = not $CampfireMenu.visible
-	if(extinguish):
-		$campfireSprite.play('extinguished')
-	else:
-		$campfireSprite.play('lit')
+	if not extinguish:
+		# save game and heal player to max hp
+		Save.save_game()
+		Player.instance.health_component.heal(Player.instance.health_component.max_health)
+
+		# extinguish campfire
+		extinguish = true
+		used_list.append(get_path())
+		%Sprite.play('extinguished')
+		%Light.visible = false
 	
-	
-	
-	Save.save_game()#Self Explanatory
-	Player.instance.health_component.heal(max_health)#Calls the player's health component and heals
-	
-	
+	# open campfire menu
+	var menu := preload("res://menu/campfire_menu/campfire_menu.tscn").instantiate()
+	add_child(menu)
