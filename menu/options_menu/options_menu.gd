1afebd4c239b674392dc5dd26f16b2780093cf84
Ripcord17 <jessupgravitt@gmail.com>
2025-11-05 22:00:40 -0500

    ok NOW I added changing values to the sliders LOL

--- menu/options_menu/options_menu.gd
+++ menu/options_menu/options_menu.gd
@@ -2,22 +2,28 @@ extends Control
 @export var back_button : Button
 
 @onready var bus := AudioServer.get_bus_index("Master")
+var linear_music := db_to_linear(AudioServer.get_bus_volume_db(bus))
 
-# Placeholder values btw
-@export var music_value := 50
+# Placeholder value for vfx
+@export var music_value := linear_music * 100
 @export var vfx_value := 50
+
 
 func _ready() -> void:
 	%Music.value = music_value
 	%VFX.value = vfx_value
 	
-# 
+	%MusicPercentage.text = str(int(music_value)) + "%"
+	%VFXPercentage.text = str(int(vfx_value)) + "%"
+
+	# need to change value of sound. uhhhh
+	# is there a better way to do the int calling? it looks... ass LOL
+
 func _on_music_value_changed(_value: float) -> void:
 	music_value = %Music.value
-	%MusicPercentage.text = str(music_value) + "%"
+	%MusicPercentage.text = str(int(music_value)) + "%"
 	
 	
-
 func _on_vfx_value_changed(_value: float) -> void:
 	vfx_value = %VFX.value
-	%VFXPercentage.text = str(vfx_value) + "%"
+	%VFXPercentage.text = str(int(vfx_value)) + "%"
