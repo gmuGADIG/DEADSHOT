e97f92577d63b4ebaef73ffdde12f03c222a7200
studyemail575-dotcom <studyemail575@gmail.com>
2025-11-05 21:26:29 -0500

    [PROG] Interactable - Ammo Box
    -Added collision with the ammo box which will make it disappear
    
    -adds a random amount from 5-20 ammo
    
    -displays the amount of bullets and the number of bullets the player picks up

--- world/interactables/Ammo_Box_Pickup/Ammo_Box.gd
+++ world/interactables/Ammo_Box_Pickup/Ammo_Box.gd
@@ -1,9 +1,19 @@
 extends Area3D
 
-@export var ammo: int
+
+
+@export var Num_Of_Ammo_In_Box: int
+#@export var Num_Of_Ammo_In_Box: int
 func _on_body_entered(body: Node3D) -> void:
 	# player will pick up ammo when it collides
 	if body is Player:
+		Num_Of_Ammo_In_Box=randf_range(5,20)
+		Player.instance.get_node(Player.gun_name).reserve_ammo+=Num_Of_Ammo_In_Box
+		#print("Ammo Box Picked Up and got: ")
+		#print(Num_Of_Ammo_In_Box)
+		#print(Player.instance.get_node(Player.gun_name).reserve_ammo)
 		
-		print("ammo Box picked up")
+		print("Ammo Box Picked Up and got " + str(Num_Of_Ammo_In_Box)+" Ammo")
+		print("You Have "+str(Player.instance.get_node(Player.gun_name).reserve_ammo)+" Ammo")
 		
+		queue_free()
