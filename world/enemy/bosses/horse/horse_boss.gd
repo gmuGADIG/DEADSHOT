784f1b21b82373527c060a908ec6d46812376d6e
Tushar <45638547+MistOfJudgement@users.noreply.github.com>
2025-11-05 21:50:28 -0500

    roaming and fixed horse stomp

--- world/enemy/bosses/horse/horse_boss.gd
+++ world/enemy/bosses/horse/horse_boss.gd
@@ -18,6 +18,8 @@ var target:Vector3
 
 @onready var shaker: SpriteShaker = %SpriteShaker
 
+@export var floor_area: Path3D
+
 var current_action: StringName = ""
 #endregion
 
@@ -37,8 +39,15 @@ func _ready() -> void:
 
 func idle() -> void: pass
 
+func get_2d_angle(from: Vector3, to: Vector3) -> float:
+	var cross := from.cross(to)
+	var dot := from.dot(to)
+	var angle := atan2(cross.y, dot)
+	return angle
 func stomp_fire_attack() -> void:
 	print("stomp fire")
+	aggro = AggroState.ATTACKING
+	should_move = false
 	for i in range(num_shots_in_spread):
 		var bullet_reference: Node3D = rock_obj.instantiate()
 		add_sibling(bullet_reference)
@@ -47,13 +56,13 @@ func stomp_fire_attack() -> void:
 
 		var target_position: Vector3 = player.global_position
 		var direction: Vector3 = (target_position - global_position).normalized()
-		var base_angle: float = direction.angle_to(Vector3.FORWARD)
+		var base_angle: float = get_2d_angle(Vector3.FORWARD, direction)
 		var angle_offset: float = spread_angle * ((float(i) / (num_shots_in_spread - 1)) - 0.5)
 		var final_angle: float = base_angle + deg_to_rad(angle_offset)
 		var final_direction: Vector3 = -Vector3(sin(final_angle), 0, cos(final_angle)).normalized()
 		bullet_reference.set_target(global_position + final_direction * 10)
 
-		print("Fired rock at angle offset: %f" % angle_offset)
+		print("Fired rock at angle offset: %f" % rad_to_deg(final_angle))
 
 #region Longhorn_funcs
 func longhorn_charge_ready() -> void:
@@ -76,7 +85,9 @@ func longhorn_charge_attack() -> void:
 func _on_charge_timer_timeout() -> void:
 	if not is_charging: 
 		current_action = ""
+		roam()
 		pick_action()
+		
 	
 func _on_hurter_box_area_entered(area: Area3D) -> void:
 	if area is Hurtbox:
@@ -94,6 +105,7 @@ func action_finished(anim_name: StringName) -> void:
 	else:
 		current_action = ""
 		super.action_finished(anim_name)
+		roam()
 		
 func longhorn_process() -> void:
 	
@@ -108,4 +120,34 @@ func longhorn_process() -> void:
 func attack() -> void:
 	if (is_charging):
 		longhorn_process()
+
+func get_bounds() -> AABB:
+	var minPoint: Vector3 = floor_area.curve.get_point_position(0)
+	var maxPoint: Vector3 = floor_area.curve.get_point_position(0)
+	for i in range(floor_area.curve.get_point_count()):
+		var point: Vector3 = floor_area.curve.get_point_position(i)
+		minPoint.x = min(minPoint.x, point.x)
+		minPoint.y = min(minPoint.y, point.y)
+		minPoint.z = min(minPoint.z, point.z)
+		maxPoint.x = max(maxPoint.x, point.x)
+		maxPoint.y = max(maxPoint.y, point.y)
+		maxPoint.z = max(maxPoint.z, point.z)
+	return AABB(minPoint, maxPoint - minPoint)
+
+func roam() -> void:
+	var bounds: AABB = get_bounds()
+	var random_x: float = randf_range(bounds.position.x, bounds.position.x + bounds.size.x)
+	var random_z: float = randf_range(bounds.position.z, bounds.position.z + bounds.size.z)
+	var target_pos: Vector3 = Vector3(random_x, global_position.y, random_z)
+	set_movement_target(target_pos)
+	should_move = true
+	aggro = AggroState.HOSTILE
+	if not navigation_agent.is_target_reachable():
+		roam() #try again
+	
+
+func hostile() -> void:
+	if navigation_agent.is_navigation_finished():
+		pick_action()
+	
 #endregion
