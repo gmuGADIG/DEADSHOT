2dfeed89dc66a6b1eb0d9972c9c17b187c69c661
Ripcord17 <jessupgravitt@gmail.com>
2025-11-05 21:46:58 -0500

    Added changing percentage values next to Music and VFX sliders.

--- menu/options_menu/options_menu.gd
+++ menu/options_menu/options_menu.gd
@@ -1,4 +1,25 @@
 extends Control
 @export var back_button : Button
 
-func back()
+@onready var bus := AudioServer.get_bus_index("Master")
+
+# Placeholder values btw
+@export var music_value := 50
+@export var vfx_value := 50
+
+func _ready() -> void:
+	%Music.value = music_value
+	%VFX.value = vfx_value
+	
+# 
+func _on_music_value_changed(_value: float) -> void:
+	music_value = %Music.value
+	%MusicPercentage.text = str(music_value) + "%"
+	
+	
+
+func _on_vfx_value_changed(_value: float) -> void:
+	vfx_value = %VFX.value
+	%VFXPercentage.text = str(vfx_value) + "%"
+	
+	
