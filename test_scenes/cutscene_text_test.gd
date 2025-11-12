fcb9f76fc2df2020a688e297a670213b575a661f
Michael Campbell <me@micycle.dev>
2025-11-12 17:55:43 -0500

    make cutscene text system a bit more modular

--- test_scenes/cutscene_text_test.gd
+++ test_scenes/cutscene_text_test.gd
@@ -2,22 +2,32 @@ extends Node2D
 @onready var tap_button : TextureButton = $TextureButton
 @onready var animator : AnimationPlayer = $AnimationPlayer
 @onready var text : Node2D = $Text
-@export var timeline:DialogTimeline
+
+@export var timelines: Array[DialogTimeline]
+#@export var timeline:DialogTimeline
+
+func play_timeline(timeline_idx: int) -> void:
+	animator.pause()
+	Dialog.play(timelines[timeline_idx])
+	await text_finished()
+	animator.play()
 
 func _ready()->void:
 	tap_button.hide()
 	
-	animator.play("before_dialogue")
+	animator.play("cutscene")
 	await animator.animation_finished
-
-	Dialog.play(timeline)
-	await text_finished()
-	
-	animator.play("after_dialogue")
-	await animator.animation_finished
-
 	await show_advance_prompt()
 	get_tree().change_scene_to_file("res://world/levels/desert_intro/level_desert_intro.tscn")
+	
+	#animator.play("before_dialogue")
+	#await animator.animation_finished
+#
+	#Dialog.play(timeline)
+	#await text_finished()
+	#
+	#animator.play("after_dialogue")
+	#await animator.animation_finished
 
 func text_finished() -> void:
 	while Dialog.panel.visible:
