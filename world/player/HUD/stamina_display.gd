6b6cc2ba43d8ab0879b69eb4f62738ded178b98e
Justin1L8 <justin.w.langdon@gmail.com>
2025-11-12 18:08:31 -0500

    Stamina hides while full

--- world/player/HUD/stamina_display.gd
+++ world/player/HUD/stamina_display.gd
@@ -5,6 +5,8 @@ func _ready() -> void:
 	Global.player_stamina_changed.connect(set_stamina)
 
 func set_stamina(stamina: float) -> void:
+	visible = stamina < 3.0
+	
 	for blip: ProgressBar in get_children():
 		blip.value = clamp(stamina, 0, 1)
 		stamina -= 1.0
