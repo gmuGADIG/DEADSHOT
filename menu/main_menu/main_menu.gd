241139882d53d36a51d9d0dd1b1f35a00fd597bc
Ethan Nguyen <72627687+cooki-chan@users.noreply.github.com>
2025-11-12 19:59:37 -0500

    things

--- menu/main_menu/main_menu.gd
+++ menu/main_menu/main_menu.gd
@@ -1,19 +1,18 @@
 extends Control
 
 func _on_new_save_button_pressed() -> void:
-	#Load New Save
-	print("New Save Loaded")
 	get_tree().change_scene_to_file("res://menu/cutscenes/intro/intro_cutscene.tscn")
 
 func _on_load_save_button_pressed() -> void:
-	#Load Save
-	print("Save Loaded")
+	if Save.save_file_exists():
+		Save.load_game()
+	else:
+		_on_new_save_button_pressed()
 
 func _on_options_button_pressed() -> void:
-	#Load Options Menu Scene
+	# TODO: Load Options Menu Scene
 	print("Options Opened")
 
 func _on_quit_button_pressed() -> void:
 	#Quit Game
-	print("Game Quit")
 	get_tree().quit()
