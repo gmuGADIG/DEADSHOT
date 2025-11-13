aec57ce88a8b98d745230672ebc88230bc3aa8ea
Max <max.fowlkes@gmail.com>
2025-11-13 17:05:31 -0500

    cleanup!
    this is probably done but i'm waiting on Alec's opinion

--- world/player/player_animation.gd
+++ world/player/player_animation.gd
@@ -1,5 +1,5 @@
 extends AnimatedSprite3D
-# This script controls the player's sprite animation based on the player movement state and shooting.
+# This script controls the player's sprite animation and facing direction based on the player movement state and shooting.
 
 
 ## Player reference to check states
@@ -8,11 +8,12 @@ extends AnimatedSprite3D
 var gun: Gun
 
 func _ready() -> void:
-	#TODO: When the player changes their gun, this script will need to be told to get_gun() again! So will the player.
+	#TODO: When the player changes their gun, this script will need to be told to get_gun() again and reconnect the signal! So will the player.
 	gun = player.get_gun()
 	gun.fired.connect(_on_gun_fired)
 	player.player_state_changed.connect(_on_player_state_change)
 	animation_finished.connect(_on_animation_finished)
+
 
 func _process(_delta: float) -> void:
 	updateMovementAnimation(player.current_state)
@@ -23,16 +24,13 @@ func _process(_delta: float) -> void:
 func _on_player_state_change() -> void:
 	updateMovementAnimation(player.current_state)
 	
-
 ## Update if the player is walking or rolling
 func updateMovementAnimation(state: Player.PlayerState) -> void:
 	match state:
 		player.PlayerState.WALKING:
 			updateSpriteAnimation("walking")
-			#TODO: Flip sprite direction based on player direction
 		player.PlayerState.ROLLING:
 			updateSpriteAnimation("rolling")
-			#TODO: Flip sprite direction based on player direction
 
 func updateSpriteAnimation(animationName: String) -> void:
 	match animationName:
@@ -43,9 +41,13 @@ func updateSpriteAnimation(animationName: String) -> void:
 				return
 		"walking":
 			# Similarly, if the system tries to play the walking animation, it'll be stopped if
-			# the player is in the middle of shooting.
-			if animation=="shooting":
+			# the player is in the middle of shooting. Shooting takes priority.
+			if animation=="shooting" || player.velocity == Vector3.ZERO:
 				return
+		"shooting":
+			# We stop the sprite before playing the "shooting" animation. This means that if the player needs
+			# to shoot before the animation is finished, the sprite will actually shoot again instead of continuing.
+			stop()
 	play(animationName)
 
 
@@ -56,21 +58,33 @@ func _on_animation_finished() -> void:
 		animation="idle"
 	updateMovementAnimation(player.current_state)
 
+
 func _on_gun_fired() -> void:
-	#if/else statement to update the sprite's direction according to direction shot towards. skips if player is aiming perfectly upwards
-	if player.aim_dir().x != 0: 
-		scale.x = 0.39 * player.aim_dir().x/abs(player.aim_dir().x)
+	# The player needs that feedback to see the direction they're shooting in. No matter how they're walking,
+	# we flip the sprite to face towards their line of fire.
+	if player.aim_dir().x >= 0: 
+		flip_h=false
 	else:
-		return
+		flip_h=true
 	updateSpriteAnimation("shooting")
 
-
+## Flips sprite based on movement direction
 func checkSpriteDirection() -> void:
+	# We skip updating the direction if the player is shooting. We want the player to face their line of fire
+	# more than we want them to face their movement direction.
+	if animation=="shooting": return
+	
+	# Dot product of two normal vectors! 
+	# The player sprite art is RIGHT FACING, i.e. always Vector2.RIGHT.
+	# We're checking if the player's movement is in the same direction as that or not. If not, we flip it.
 	var playerDirection: Vector2 = Vector2(player.velocity.x, player.velocity.z).normalized()
-	var directionDifference: float = Vector2.RIGHT.dot(playerDirection)
-	#updates the sprite's direction if the player isn't standing still nor shooting
-	#TODO: fix lighting issue when the sprite is scaled negatively
-	if (directionDifference != 0) && (animation != "shooting"):
-		scale.x = 0.39 * directionDifference/abs(directionDifference)
-	else:
-		return
+	var directionDifference: int = int(roundf(Vector2.RIGHT.dot(playerDirection)))
+	match directionDifference:
+		-1:
+			flip_h=true
+		0:
+			flip_h=false
+		1:
+			flip_h=false
+		_:
+			printerr("player_animation.gd directionDifference is out of bounds!")
