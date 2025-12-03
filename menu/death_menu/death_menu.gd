e402092d60ed321c20ac69e4530951a0cacb9027
Justin1L8 <justin.w.langdon@gmail.com>
2025-12-03 14:06:58 -0500

    Added save system into death menu

--- menu/death_menu/death_menu.gd
+++ menu/death_menu/death_menu.gd
@@ -1,15 +1,10 @@
 extends Node2D
 
 func _on_load_save_button_pressed() -> void:
-	#Load previous save (Need to incorporate saves for this to work, so it just resets to the intro for now)
-	get_tree().change_scene_to_file("res://menu/cutscenes/intro/intro_cutscene.tscn")
-
+	Save.load_game()
 
 func _on_main_menu_button_pressed() -> void:
-	#return to main menu
 	get_tree().change_scene_to_file("res://menu/main_menu/main_menu.tscn")
 
-
 func _on_quit_button_pressed() -> void:
-	#quit game
 	get_tree().quit()
