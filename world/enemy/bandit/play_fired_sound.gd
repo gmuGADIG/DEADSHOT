1e45f84643db1f0f338370737f27a2dd3c8a2b68
Michael Campbell <me@micycle.dev>
2025-12-03 17:19:50 -0500

    refactored sound code

--- world/enemy/bandit/play_fired_sound.gd
+++ world/enemy/bandit/play_fired_sound.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var fireSound:AudioStreamPlayer3D = $"../Sounds/BanditFire"
+@onready var fireSound: AudioStreamPlayer3D = %BanditFire
 
 func _on_enemy_bandit_enemy_fired() -> void:
 	fireSound.play()
