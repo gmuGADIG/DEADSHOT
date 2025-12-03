45437c43b726575006c81ee33c00cd61d000d080
Justin1L8 <justin.w.langdon@gmail.com>
2025-12-03 13:40:46 -0500

    Removed tmp file, removed excess blank lines

--- world/enemy/bosses/horse/horse_splash.gd
+++ world/enemy/bosses/horse/horse_splash.gd
@@ -5,20 +5,15 @@ extends Node3D
 @onready var timer : Timer = $Timer
 @onready var y_pos : float = $Splash.position.y
 
-
 var age : float = -1
 
 func prime() -> void:
 	age = 0
 
-
-
 func _ready() -> void:
 	# Connect the timeout signal to a function in this script
 	timer.wait_time = delay # Set wait time
 	timer.one_shot = true # Make it run only once
-	
-	
 
 func _process(delta: float) -> void:
 	if not timer.is_stopped():
@@ -26,11 +21,6 @@ func _process(delta: float) -> void:
 			age += delta
 		
 		$Splash.position.y = y_pos + 3 * splash_curve.sample(1-(timer.time_left / delay))
-		 
-	
-	
-	
-
 
 func _on_damage_area_area_entered(area: Area3D) -> void:
 	if area is Hurtbox:
@@ -42,7 +32,6 @@ func Area_entered(_area: Area3D) -> void:
 	print("Chunk collision")
 	#_area.queue_free()
 	pass # Replace with function body.
-
 
 func timer_timeout() -> void:
 	queue_free()
