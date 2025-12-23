007d8cdb58b22ac01335e1565579fd8fa938f80e
KaratePikachu <80354180+KaratePikachu@users.noreply.github.com>
2025-12-22 23:15:31 -0500

    Sheriff Star

--- world/enemy/sheriff/sheriff.gd
+++ world/enemy/sheriff/sheriff.gd
@@ -1,24 +1,22 @@
 extends Wilder
 
-enum SheriffAttack {
-	NORMAL,
-	STAR,
-	
-}
 @export var star : PackedScene
 
 func attack() -> void:
 	while true:
-		var attack_type : SheriffAttack = SheriffAttack.values().pick_random()
-		match attack_type:
-			SheriffAttack.NORMAL:
-				await get_tree().create_timer(randf_range(timeBetweenShotsMin,timeBetweenShotsMax), false).timeout
-				if process_mode == ProcessMode.PROCESS_MODE_DISABLED: continue
-				shootBullet()
-			SheriffAttack.STAR:
-				await get_tree().create_timer(randf_range(timeBetweenShotsMin,timeBetweenShotsMax), false).timeout
-				if process_mode == ProcessMode.PROCESS_MODE_DISABLED: continue
-				shootStar()
+		var rng : int = randi_range(1,100)
+		if rng <= 80 :
+			await get_tree().create_timer(randf_range(timeBetweenShotsMin,timeBetweenShotsMax), false).timeout
+			if process_mode == ProcessMode.PROCESS_MODE_DISABLED: continue
+			shootBullet()
+		else:
+			var old_speed := movement_speed
+			movement_speed = 0
+			await get_tree().create_timer(1, false).timeout
+			if process_mode == ProcessMode.PROCESS_MODE_DISABLED: continue
+			shootStar()
+			await get_tree().create_timer(2.0, false).timeout
+			movement_speed = old_speed
 
 func shootStar() -> void:
 	var new_star : = star.instantiate()
