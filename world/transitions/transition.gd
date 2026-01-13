b5b15bc1264f5e5860800b1dab9a63a260950385
Camber Boles <camberb51@gmail.com>
2026-01-13 15:52:43 -0500

    Transition area fades to black

--- world/transitions/transition.gd
+++ world/transitions/transition.gd
@@ -1,13 +1,19 @@
 extends Area3D
+
 @export_file("*.tscn") var target_scene:String 
 @export var target_entry_point:String
+
+@onready var fade_panel : ColorRect = $Fade
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is not Player: return
 	
 	Player.update_persisting_data()
-	
 	EntryPoints.set_entry_point(target_entry_point)
+	
+	fade_panel.show()
+	fade_panel.color = Color(0,0,0,0)
+	var fade_tween : Tween = create_tween()
+	fade_tween.tween_property(fade_panel, "color", Color.BLACK, 3)
+	await fade_tween.finished
 	get_tree().change_scene_to_file(target_scene)
-	
-	
