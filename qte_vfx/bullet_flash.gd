9f76feefba4683feba54f4c78916e19cbd3dfa8d
Camber Boles <camberb51@gmail.com>
2026-01-13 12:52:28 -0500

    New files from main

---
+++ qte_vfx/bullet_flash.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+@export var noise_texture: NoiseTexture2D
+
+@onready var canvas_modulate: CanvasModulate = %BFCanvasModulate
+@onready var sprite: Sprite2D = %BFSprite
+
+func flash() -> void:
+	canvas_modulate.color.a = 1.
+	sprite.position = get_viewport().get_mouse_position()
+
+	show()
+	await create_tween() \
+		.set_ignore_time_scale() \
+		.tween_property(canvas_modulate, "color:a", 0, 0.15) \
+		.finished
+
+	hide()
+	noise_texture.noise.seed += 1
---
+++ qte_vfx/qte_vfx.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+@export var engine_timescale := 1.:
+	set(v):
+		Engine.time_scale = v
+	get:
+		return Engine.time_scale
+
+
+var active := false
+
+
+func _process(_delta: float) -> void:
+	%AnimationPlayer.speed_scale = 1. / Engine.time_scale
+
+
+func start() -> void:
+	%AnimationPlayer.play("woosh_in")
+	%Greyscale.start()
+	active = true
+
+
+func end() -> void:
+	%AnimationPlayer.play("woosh_out")
+	active = false
