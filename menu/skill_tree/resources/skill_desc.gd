e44f4fbaa0a790e4c18c673e0b608a2601934511
Justin1L8 <justin.w.langdon@gmail.com>
2026-01-13 17:07:09 -0500

    Assigned icons to all skill descriptions

--- menu/skill_tree/resources/skill_desc.gd
+++ menu/skill_tree/resources/skill_desc.gd
@@ -1,8 +1,9 @@
-extends Resource
 class_name SkillDesc
+extends Resource
 
 @export var skill_name : String
-@export var skill_image : Texture
+@export var icon1 : SkillIcon
+@export var icon2 : SkillIcon
 @export_multiline var skill_description : String
 @export var skill_meat_cost : int
 @export var skill_uid : SkillSet.SkillUID
