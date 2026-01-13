e78d1f36a80003b966bed328892bc0df8564c165
Ripcord17 <jessupgravitt@gmail.com>
2026-01-13 02:00:08 -0500

    Added save options implementation

--- menu/options_menu/options_menu.gd
+++ menu/options_menu/options_menu.gd
@@ -30,12 +30,14 @@ func _on_music_value_changed(_value: float) -> void:
 
 	var perceived := clampf(music_value / 100.0, 0.0, 1.0)
 	MainMusicPlayer.set_master_loudness(perceived)
+	OptionsSave.save_options()
 	
 func _on_sfx_value_changed(_value: float) -> void:
 	sfx_value = %SFX.value
 	%SFXPercentage.text = str(int(sfx_value)) + "%"
 
 	AudioServer.set_bus_volume_linear(sfx_bus, sfx_value / 100.)
+	OptionsSave.save_options()
 
 func _on_back_button_pressed() -> void:
 	$AnimationPlayer.play_backwards("open")
