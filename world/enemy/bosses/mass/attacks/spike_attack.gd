05b2522deeac417d29e989a1a9397cfb2a846973
connorheyz <connorheyz@gmail.com>
2026-01-14 20:30:23 -0500

    Add flesh stream sound effect to The Mass boss (#712)
    Introduced a new flesh stream sound effect and integrated it into The Mass boss's chunk attack. Added related audio files, scene, and node, and updated spike attack sound logic to prevent repeated playback.

--- world/enemy/bosses/mass/attacks/spike_attack.gd
+++ world/enemy/bosses/mass/attacks/spike_attack.gd
@@ -3,20 +3,20 @@ class_name Spike extends Node3D
 @export var delay : float = 0.75 ##Time to wait before sending up the spike
 @export var spike_curve : Curve ##How the spike moves over time
 
-
+var debounce: bool = false
 
 var age : float = -1
 
 func prime() -> void:
 	age = 0
 
-func _ready() -> void:
-	if $SpikePierceSound:
-		$SpikePierceSound.play()
-
 func _process(delta: float) -> void:
 	if age >= delay: ##Do attack
 		$Spike.position.y = spike_curve.sample(age-delay)
+		
+		if $SpikePierceSound and not debounce:
+			debounce = true
+			$SpikePierceSound.play()
 	
 	if age >= 0:
 		age += delta
