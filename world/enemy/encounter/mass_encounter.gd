b433fb5d1fa6d3eddb3336adbd5e96f37ff0f164
Justin1L8 <justin.w.langdon@gmail.com>
2026-01-15 00:47:13 -0500

    Mass spawn animation

---
+++ world/enemy/encounter/mass_encounter.gd
@@ -0,0 +1,18 @@
+@tool
+extends Encounter
+
+@export var anim: AnimationPlayer
+@export var pre_timeline: DialogTimeline
+
+func start_encounter() -> void:
+	Player.instance._on_interaction_started()
+	anim.play("start")
+	await anim.animation_finished
+	MainCam.instance.process_mode = Node.PROCESS_MODE_DISABLED
+	
+	Dialog.play(pre_timeline)
+	await Dialog.timeline_ended
+	
+	MainCam.instance.process_mode = Node.PROCESS_MODE_INHERIT
+	super.start_encounter()
+	Player.instance._on_interaction_ended()
