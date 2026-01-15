805254b9aac96b47ec5b3fe78bcc283358aa803d
Justin1L8 <justin.w.langdon@gmail.com>
2026-01-15 02:09:43 -0500

    Added mass hp bar

--- world/enemy/encounter/mass_encounter.gd
+++ world/enemy/encounter/mass_encounter.gd
@@ -3,6 +3,7 @@ extends Encounter
 
 @export var anim: AnimationPlayer
 @export var pre_timeline: DialogTimeline
+@export var mass: TheMass
 
 func start_encounter() -> void:
 	Player.instance._on_interaction_started()
@@ -15,4 +16,5 @@ func start_encounter() -> void:
 	
 	MainCam.instance.process_mode = Node.PROCESS_MODE_INHERIT
 	super.start_encounter()
+	Global.boss_spawned.emit(mass)
 	Player.instance._on_interaction_ended()
