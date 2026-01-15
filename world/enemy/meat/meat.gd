7f13dad63dc012f59435ae4f8c0cd1a1892d802e
Michael Campbell <me@micycle.dev>
2026-01-14 20:45:23 -0500

    Revert "set meat gain to 3"
    This reverts commit 7692f06fb559db01dceb7e88d85e64d92e576fb5.

--- world/enemy/meat/meat.gd
+++ world/enemy/meat/meat.gd
@@ -7,7 +7,7 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		return
 	
 	print("Currecny Colletced")
-	Global.meat_currency += 3
+	Global.meat_currency += 10
 	print(Global.meat_currency)
 	
 	var label_instance := Label3D.new()
